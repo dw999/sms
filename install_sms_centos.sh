a8cb35e88e392f931c0b994606d81fbd26346f86
root <davidwebb3194@gmail.com>
2019-03-13 13:30:08 +0800

    Refine SMS installation script

--- install_sms_centos.sh
+++ install_sms_centos.sh
@@ -39,8 +39,10 @@ fi
 #-- Check whether SMS has already been installed. If it is, stop proceed. --#
 if [ -d "/www/pdatools" ] || [ -d "/www/itnews" ] || [ -d "/www/perl_lib" ]
 then
-  echo "It seems that SMS has been installed (at least it has been tried before). Therefore, directory '/www'"
-  echo "has already existed. If SMS installation with error and you need to try again, you have to delete '/www'"
+  echo "It seems that SMS has been installed (at least it has been tried before). Therefore, sub-directories 'pdatools', "
+  echo "'itnews' or 'perl_lib' has/have already existed on directory '/www'."
+  echo ""
+  echo "If SMS installation is failure and you need to try again, you have to delete those sub-directories on '/www'"
   echo "manually and re-run installation script 'install_sms_centos.sh'."
   echo ""
   echo "Note: Re-run installation script in a production SMS server will damage all messages on it."
